c3a437d8fc12900d7e77e819b4bc2c9a640f1bef
YueSyuan <la92142850910@gmail.com>
2021-05-05 16:27:49 +0800

    Init JpaArticleDemo

---
+++ src/main/java/com/example/demo/entity/Article.java
@@ -0,0 +1,24 @@
+package com.example.demo.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+
+@Entity
+@Data
+public class Article extends BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    @NotEmpty(message = "標題不能回空")
+    private String title;
+
+    @Column(nullable = false)
+    private String body;
+
+    private long view;
+}
